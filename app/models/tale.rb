368f7becdbe6d3783a59ebc8d1c22d0ac12b8c7e
Isaac Simon <ijsimon@sbcglobal.net>
2017-11-22 22:14:39 +0000

    edited tale model

--- app/models/tale.rb
+++ app/models/tale.rb
@@ -2,15 +2,6 @@ class Tale < ApplicationRecord
     belongs_to :genre
     has_many :comments, dependent: :destroy
     
-    after_validation :validate_body
-    
     validates :title, length: { minimum: 1 }, presence: true
-    
-    
-    def validate_body
-        if body.count(" ") < 3 
-         return error
-        end
-    end
 
 end
